71d5efee428b99e49d7e0c7824451725495acf29
ianrelecker <ianrelecker@gmail.com>
2020-05-04 13:39:18 -0700

    rewritten

--- Program.cs
+++ Program.cs
@@ -1,31 +1,135 @@
-﻿using System;
+﻿// Ian Relecker
+// www.ianrelecker.com
+// ianrelecker@gmail.com
+// 
+// What this program does is it prints a 8x8 grid to the screen(Console in C#) 
+// of a chessboard where 8 Queens could all fit on the board.
+// 
+// I originally wrote this in Java but have rewritten it in C#.
+
+using System;
+
 
 namespace _8Queens
 {
     class Program
     {
-public static int checkOne(int[] array){
-        int total = 0;
-        for(int i=0; i<array.Length; i++){
-            total += array[i];
+    public static int checkOne(int[] array){
+            int total = 0;
+            for(int i=0; i<array.Length; i++){
+                total += array[i];
+            }
+            return total;
         }
-        return total;
-    }
-    public static int checkRC(int[][] array){
+
+    public static int checkRC(int[,] array){
         int total = 0;
-        for(int i=0; i<array.Length; i++){
-            for(int a=0; a<array.Length; a++){
-                total += array[i][a];
+        for(int i=0; i<8; i++){
+            for(int a=0; a<8; a++){
+                total += array[i,a];
             }
         }
         return total;
     }
+
     public static int randNumF(){
         Random num = new Random();
         return num.Next(0,8);
     }
-    public static void Main(string[] args){
-        Console.WriteLine(randNumF());
+
+    public static bool diaChecker(int x, int y, int[,] chessboardM){
+        bool isItThere = false;
+
+        int i =0;
+        while(x+i<=7&&y-i>=0){
+            if(chessboardM[y-i,x+i] == 1) {
+                isItThere = true;
+                break;
+            }
+            i++;
+        }
+        i=0;
+        while(y-i>=0&& x-i>=0){
+            if (chessboardM[y-i,x-i] == 1){
+                isItThere = true;
+                break;
+            }
+            i++;
+        }
+        i=0;
+        while(y+i<=7&&x+i<=7){
+            if(chessboardM[y+i,x+i] ==1){
+                isItThere = true;
+                break;
+            }
+            i++;
+        }
+        i=0;
+        while(x-i>=0&&y+i<=7){
+            if(chessboardM[y+i,x-i] == 1){
+                isItThere = true;
+                break;
+            }
+            i++;
+        }
+        return isItThere;
     }
+
+    public static void Main(String[] args) {
+        int[,] chessboard = new int[8, 8];
+
+        while(checkRC(chessboard) < 8){
+            int counter = 0;
+            chessboard = new int[8,8];
+            int[] columnHolder = new int[8];
+            int[] rowHolder = new int[8];
+            while(checkRC(chessboard) < 8){
+                if(counter == 10000){
+                    break;
+                }
+                int testingNowX = randNumF();
+                int testingNowY = randNumF();
+                counter++;
+                for(int i=0; i<8; i++){
+                    for(int a=0; a<8; a++){
+                        rowHolder[a] = chessboard[testingNowY,a];
+                    }
+                if(checkOne(rowHolder) !=1 && columnHolder[testingNowX] != 1 && !diaChecker(testingNowX, testingNowY, chessboard)){
+                    chessboard[testingNowY,testingNowX] = 1;
+                    columnHolder[testingNowX] = 1;
+                }
+                }
+            }
+        }
+        Console.WriteLine("Complete! Found spot for all 8 queens!");
+        for(int i = 0; i <8; i++)
+        {
+            for(int a=0; a<8; a++)
+            {
+                Console.Write(chessboard[i,a]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+        // public static void Main(string[] args)
+        // {
+        //     int[,] apple = new int[8,8];
+        //     int counter = 0;
+        // for(int i=0; i<8; i++){
+        //     for(int a=0; a<8; a++){
+        //         apple[i,a] = counter;
+        //         counter++;
+        //     }
+        // }
+
+        // for(int i=0; i<8; i++){
+        //     for(int a=0; a<8; a++){
+        //         Console.Write(apple[i,a]);
+        //     }
+        //     Console.WriteLine();
+        // }
+        // }
+
     }
 }
